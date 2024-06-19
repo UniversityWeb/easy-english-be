8a22fd4e6da734c5c28d35dd2a8c73b2fa3038e0
AnhAnNe <info.vananne@gmail.com>
2024-06-19 23:51:13 +0700

    creating project with basic configuration

---
+++ src/main/java/com/universityweb/EasyEnglishBeApplication.java
@@ -0,0 +1,13 @@
+package com.universityweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyEnglishBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyEnglishBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universityweb/EasyEnglishBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universityweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyEnglishBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
