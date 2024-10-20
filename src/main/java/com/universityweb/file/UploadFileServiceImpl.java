d5431ae960ef330f6906b96c3702c5a6a145cfc3
AnhAnNek <info.vananne@gmail.com>
2024-10-21 02:26:41 +0700

    fix upload file cannot run

--- src/main/java/com/universityweb/file/UploadFileServiceImpl.java
+++ src/main/java/com/universityweb/file/UploadFileServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Service
-public class UploadFileServiceImpl {
+public class UploadFileServiceImpl implements UploadFileService {
     @Autowired
     private Cloudinary cloudinary;
 
